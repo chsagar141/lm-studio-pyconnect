5343cc54b27ce3f7e24a8dc26bcef567975fda34
CH SAGAR <chsagar141@gmail.com>
2025-03-06 10:42:08 +0530

    Update openAi.py

--- openAi.py
+++ openAi.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 # Point to the local server
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
-
+# add prompt at near context
 completion = client.chat.completions.create(
     model="model-identifier",
     messages=[
